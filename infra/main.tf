60cabb6c2e1e708184831457864a0c3f60d8375c
michaellangbein <michael_langbein@hotmail.de>
2024-11-22 21:14:13 +0100

    source_code object has to have the same name as actual file

--- infra/main.tf
+++ infra/main.tf
@@ -28,14 +28,14 @@ resource "google_project_service" "cloud_build_api" {
 }
 
 #------------------------------------------------------------------------------------------------------------
-# INFRASTUCTURE
+# INFRASTRUCTURE
 #------------------------------------------------------------------------------------------------------------
-
 
 # pubsub
 resource "google_pubsub_topic" "heartbeat_topic" {
   name = "heartbeat_topic"
 }
+
 resource "google_pubsub_subscription" "heartbeat_subscription" {
   name  = "heartbeat_subscription"
   topic = google_pubsub_topic.heartbeat_topic.name
@@ -54,33 +54,26 @@ resource "google_cloud_scheduler_job" "clock" {
   }
 }
 
-# # output bucket
-# resource "google_storage_bucket" "data_bucket" {
-#   name     = "lst_data_bucket"
-#   location = var.region
-# }
-
-
-
-
-
 # bucket for function-code
 resource "google_storage_bucket" "source_code_bucket" {
   name     = "lst_source_code_bucket"
   location = var.region
 }
+
 # zipping function code for upload
 resource "null_resource" "package_function" {
   provisioner "local-exec" {
-    command = "cd ../WatchData && zip -r functionSource.zip ./*"
+    command = "cd ../WatchData && zip functionSource.zip main.py requirements.txt"
   }
   triggers = {
-    file_hash = "${md5(file("../WatchData/main.py"))}"
+    file_hash_main = "${md5(file("../WatchData/main.py"))}"
+    file_hash_requirements = "${md5(file("../WatchData/requirements.txt"))}"
   }
 }
+
 # function-code
 resource "google_storage_bucket_object" "source_code" {
-  name   = "source_code"
+  name   = "functionSource.zip"
   bucket = google_storage_bucket.source_code_bucket.id
   source = "../WatchData/functionSource.zip"
 }
@@ -100,4 +93,3 @@ resource "google_cloudfunctions_function" "watch_data" {
   timeout               = 60
   environment_variables = {}
 }
-
