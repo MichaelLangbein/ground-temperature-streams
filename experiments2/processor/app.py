060a26206d2357eed770ec75be5de59a90f2b96d
Michael Langbein <michael_langbein@hotmail.de>
2024-12-01 16:37:46 +0100

    finally output logged out

--- experiments2/processor/app.py
+++ experiments2/processor/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
 import json
 import base64
+import logging
 
 
-app = Flask(__name__)
+# Configure logging 
+logging.basicConfig(level=logging.INFO) 
+logger = logging.getLogger('cloud-run-app') 
+app = Flask(__name__) 
+app.logger.addHandler(logging.StreamHandler()) 
+app.logger.setLevel(logging.INFO)
 
 @app.route("/")
 def hello():
@@ -13,7 +18,7 @@ def hello():
 @app.route("/echo", methods=["POST"])
 def echo():
     envelope = request.get_json() 
-    print(f"Received envelope: {json.dumps(envelope, indent=2)}") # Log the entire envelope
+    app.logger.info(f"Received envelope: {json.dumps(envelope, indent=2)}") # Log the entire envelope
     if not envelope: 
         return "Bad Request: no Pub/Sub message received", 400 
     pubsub_message = envelope.get("message") 
@@ -21,5 +26,5 @@ def echo():
         return "Bad Request: invalid Pub/Sub message format", 400 
     # Decode the Pub/Sub message data 
     data = base64.b64decode(pubsub_message.get("data", "")).decode("utf-8").strip() 
-    print(f"Received message: {data}")
+    app.logger.info(f"Received message: {data}")
     return data, 200
