bef30f30b4bdb5a0be5ca9de869277a17362efc8
michaellangbein <michael_langbein@hotmail.de>
2024-12-04 14:46:03 +0100

    download in assimilator successful

--- climate/Assimilator/runLocally.py
+++ climate/Assimilator/runLocally.py
@@ -1,24 +1,37 @@
 import os
 import zipfile
 from google.cloud import storage
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('cloud-run-app')
 
 
-bucketClient = storage.Client()
+def download(bucketName: str, blobName: str, extractedPath: str):
 
+    bucketClient = storage.Client()
 
-def getFromBucket(bucketName, blobName, targetPath):
-    bucket = bucketClient.get_bucket(bucketName)
-    blob = bucket.blob(blobName)
-    blob.download_to_filename(targetPath)
+    extractedPath = os.path.abspath(extractedPath)
+    zipFilePath = os.path.abspath("./data.zip")
+
+    if not os.path.exists(zipFilePath):
+        logger.info(f"""Downloading from bucket ({
+                    bucketName}/{blobName}) into {extractedPath}...""")
+        bucket = bucketClient.get_bucket(bucketName)
+        blob = bucket.blob(blobName)
+        blob.download_to_filename(zipFilePath)
+        logger.info("... download successful.")
+
+    if not os.path.exists(extractedPath) or len(os.listdir(extractedPath)) == 0:
+        logger.info("extracting ...")
+        with zipfile.ZipFile(zipFilePath, "r") as zf:
+            zf.extractall(extractedPath)
+        os.remove(zipFilePath)
+        logger.info("... extracting successful.")
 
 
 bucketName = "data_bucket_climate123"
 blobName = "data.zip"
-zipFilePath = "./data.zip"
 extractedPath = "./data"
 
-if not os.path.exists(zipFilePath):
-    getFromBucket(bucketName, blobName, zipFilePath)
-
-with zipfile.ZipFile(zipFilePath, "r") as zf:
-    zf.extractall(extractedPath)
+download(bucketName, blobName, extractedPath)
